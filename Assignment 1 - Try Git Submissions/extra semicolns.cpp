44c7ab304305b9d2304899aef6d208301f588070
wardathaheem1 <31860801+wardathaheem1@users.noreply.github.com>
2017-10-02 16:01:46 +0500

    Add files via upload

---
+++ Assignment 1 - Try Git Submissions/extra semicolns.cpp
@@ -0,0 +1,10 @@
+
+ #include<iostream>
+ using namespace std;
+  int main()
+  {
+  	int x;
+  	for(x=0; x<100; x++) // remember semicoln don't go after if statements,loops,or function definitions. 
+  	cout<<x;
+  	return 0;
+  }  
---
+++ Assignment 1 - Try Git Submissions/misusing the && and ll operators.cpp
@@ -0,0 +1,14 @@
+
+ #include<iostream>
+ using namespace std;
+ int main()
+  {
+  
+  	int value;
+  	do
+  	{
+  		value = 10;
+  		
+	}
+	  while(!(value==10) && ! (value ==20));   // misusing the && and || operator
+}
