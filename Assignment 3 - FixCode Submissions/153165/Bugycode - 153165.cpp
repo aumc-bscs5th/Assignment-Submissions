7fc8880b62a8b18c87e7f17a98708e518bdb30f1
Syed Arham Gillani <arhamgillani600@yahoo.com>
2017-10-02 22:24:28 +0500

    Input validation

--- Assignment 3 - FixCode Submissions/153165/Bugycode - 153165.cpp
+++ Assignment 3 - FixCode Submissions/153165/Bugycode - 153165.cpp
@@ -19,10 +19,18 @@ int main()
 	{
 		// enter another number to add
 		int nValue;
+		INPUT:
 		cout << "Enter another number:";
 		cin >> nValue;
 		cout << endl;
-
+		// 2. Input validation
+		if(cin.fail() || cin.get()!='\n')
+		{
+			cin.clear();
+			cin.ignore(99999, '\n');
+			cout << "Invalid Input!"<<endl;
+			goto INPUT;
+		}
 		// if the input number is negative...
 		if(nValue < 0)
 		{
