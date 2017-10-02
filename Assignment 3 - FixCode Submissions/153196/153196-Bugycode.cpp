9bb62250de567e429bf2401d5acb25895d82dbb6
Ahsan Javed <iahsanjaved@yahoo.com>
2017-10-02 15:53:12 +0500

    153196 Fix#4 Check and Validate input

--- Assignment 3 - FixCode Submissions/153196/153196-Bugycode.cpp
+++ Assignment 3 - FixCode Submissions/153196/153196-Bugycode.cpp
@@ -20,10 +20,19 @@ int main()
 	{
 		// enter another number to add
 		int nValue;
+	INPUT:	
 		cout << "Enter another number:";
 		cin >> nValue;
 		cout << endl;
-
+		
+		// Fix#4 Check and Validate input
+		if(cin.fail() || cin.get()!='\n')
+		{
+			cin.clear();
+			cin.ignore(99999, '\n');
+			cout << "Invalid Input!"<<endl;
+			goto INPUT;
+		}
 		// if the input number is negative...
 		if(nValue < 0)
 		{
